ed88978ce21c7b8a6a8bcc98d3ef1c3c183d6221
JobValdez <jodauran@gmail.com>
2022-07-10 18:16:57 -0400

    excerise 5 More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+myname = 'Job Valdez'
+myage = 27
+myheight = 6.2 # Inches
+myweight = 185 # lbs
+myeyes = 'Light-brown'
+myteeth = 'White'
+myhair = 'black'
+
+puts "Let's talk about #{myname}."
+puts "He's #{myheight} inches tall."
+puts "He's #{myweight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{myeyes} eyes and #{myhair} hair."
+puts "His teeth are usually #{myteeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{myage}, #{myheight}, and #{myweight} I get #{myage + myheight + myweight}."
