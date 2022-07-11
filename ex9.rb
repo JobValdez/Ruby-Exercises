3c34b8d250bae306e02e97a3e8f61fdc61971787
JobValdez <jodauran@gmail.com>
2022-07-11 15:43:13 -0400

    Exercise 9: Prints, Prints

---
+++ ex9.rb
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#You simply start the string with \n like this:
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+    There's something going on here.
+    With this weird quote
+    We'll be able to ty as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+}
