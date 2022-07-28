bd02d99ae2f77ed794ede5766a985c0a04051001
JobValdez <jodauran@gmail.com>
2022-07-28 17:58:03 -0400

    Exercise 34: Accessing Elements of Arrays01~

---
+++ ex34.rb
@@ -0,0 +1,9 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+show_numbers = numbers.length
+give_me_from_0_to_2 = animals[0..2]
+
+puts show_numbers
+puts give_me_from_0_to_2
